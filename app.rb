76ec71d286a877c2c6b07f6fe7bb7dcd28e568bd
Savannah C <sgcorrell@gmail.com>
2019-06-17 16:58:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+    enable :sessions
+    set :session_secret, "secret"
+  end
 end
